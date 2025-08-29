9a6cdcc58c8d554b0977c842b668efed60e3f1e6
ileeker <leeker@live.com>
2025-08-29 13:32:59 +0800

    Update detail.blade.php

--- resources/views/domains/detail.blade.php
+++ resources/views/domains/detail.blade.php
@@ -146,9 +146,12 @@
                                                 {{ $result['domain'] }}
                                             </a>
                                             <a href="https://{{ $result['domain'] }}" target="_blank" title="访问 {{ $result['domain'] }}">
-                                                <svg class="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                                </svg>
+                                                <svg class="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+  <rect x="3" y="6" width="12" height="12" rx="2" ry="2" stroke-width="2" stroke-linejoin="round"></rect>
+  <path d="M11 13l9-9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+  <path d="M15 4h5v5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>
+
                                             </a>
                                         </div>
                                     </td>
