0cd333426e61e6f611ccd6f32073ee576771b8b3
ileeker <leeker@live.com>
2025-09-04 17:13:13 +0800

    Update SimilarwebChange.php

--- app/Models/SimilarwebChange.php
+++ app/Models/SimilarwebChange.php
@@ -20,22 +20,36 @@ class SimilarwebChange extends Model
         // 月度变化
         'month_emv_change',
         'month_emv_trend',
+        'month_emv_growth_rate',
         
         // 季度变化
         'quarter_emv_change',
         'quarter_emv_trend',
+        'quarter_emv_growth_rate',
         
         // 半年变化
         'halfyear_emv_change',
         'halfyear_emv_trend',
+        'halfyear_emv_growth_rate',
         
         // 年度变化
         'year_emv_change',
         'year_emv_trend',
+        'year_emv_growth_rate',
     ];
 
     protected $casts = [
+        'current_emv' => 'integer',
+        'month_emv_change' => 'integer',
+        'quarter_emv_change' => 'integer',
+        'halfyear_emv_change' => 'integer',
+        'year_emv_change' => 'integer',
+        'month_emv_growth_rate' => 'float',
+        'quarter_emv_growth_rate' => 'float',
+        'halfyear_emv_growth_rate' => 'float',
+        'year_emv_growth_rate' => 'float',
         'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 
     /**
