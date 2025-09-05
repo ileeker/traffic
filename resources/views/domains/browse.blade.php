ea176fc999c136cbb74e43a3c6472be1fb83c7db
ileeker <leeker@live.com>
2025-09-05 11:15:12 +0800

    Update browse.blade.php

--- resources/views/domains/browse.blade.php
+++ resources/views/domains/browse.blade.php
@@ -25,7 +25,7 @@
                                 <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                 </svg>
-                                测试所有域名
+                                <font style="color: black;">测试所有域名</font>
                             </button>
                             <button id="stopTest" 
                                     class="px-4 py-2 bg-red-600 text-black rounded-md hover:bg-red-700 transition-colors duration-200 hidden">
