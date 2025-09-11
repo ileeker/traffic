c3a50d020d0fe0dcea7f1f878dc50d73afc29a9a
ileeker <leeker@live.com>
2025-09-11 12:33:19 +0800

    Update DomainController.php

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -6,6 +6,7 @@ use App\Models\Domain;
 use App\Models\SimilarwebDomain;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class DomainController extends Controller
 {
